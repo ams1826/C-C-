7328a9fbc52405da77cf1b3a273fcee74df0453a
ams1826 <akashsangwan1826@gmail.com>
2022-08-11 04:32:16 +0530

    p

--- missing number/missno2.cpp
+++ missing number/missno2.cpp
@@ -4,23 +4,34 @@
 #include<iostream>
 using namespace std;
 int missno(int a[], int n){
-    int x1=a[1];
-    int x2=1;
-    for(int i=1; i<n; i++){
-        x1=x1^a[i];
+    int x1=a[0] , start=1;
+    int x2=1 , ex2=n+1;
+
+    if(a[0]==0){
+        x1=a[1];
+        start=2;
+        ex2=n;
     }
 
-    for(int i=2; i<=n+1; i++)
-    {
+    for(int i=start; i<n; i++){
+        cout<<x1<<" ";
+        x1=x1^a[i];
+        cout<<x1<<endl;
+    }
+    cout<<"\n\n";
+    for(int i=2; i<=ex2; i++){
+        cout<<x2<<" ";
         x2=x2^i;
+        cout<<x2<<endl;
     }
 
     return (x1^x2);
 }
 int main(){
-    int arr[]={0,2,3,4};
+    int arr[]={2,3,4,5};
     int N=sizeof(arr)/sizeof(arr[0]);
     int miss = missno(arr,N);
+    cout<<"\n\n";
     cout<<miss;
     return 0;
 }
