46e91682bf8f3000732be216cfbc192f1509d228
ams1826 <akashsangwan1826@gmail.com>
2022-08-13 15:27:53 +0530

    p

---
+++ recursion/fibo.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int fibo(int n){
+    // cout<<n<<endl;
+    if((n==1) || (n==0))
+    return n;
+
+    else
+    return fibo(n-1)+fibo(n-2);
+}
+
+int main(){
+    int x = 5 ,i=0;
+    cout<<fibo(x);
+    // while(i < x) {
+    //   cout <<" "<<fibo(i);
+    //   i++;
+    // }
+    return 0;
+}
