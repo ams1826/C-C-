1d68f241cdca808ddb1042eb3141733ca1603c6d
ams1826 <akashsangwan1826@gmail.com>
2022-08-16 10:23:08 +0530

    p

--- tcode.cpp
+++ tcode.cpp
@@ -1,6 +1,5 @@
 //try codes for trail run only
 #include<bits/stdc++.h>
-#include<iostream>
 using namespace std;
 
 int bs(int arr[], int l, int r, int k){
@@ -11,10 +10,10 @@ int bs(int arr[], int l, int r, int k){
         if(arr[mid]==k)
         return mid;
 
-        if(arr[mid]>k)
-        return bs(arr, l, mid-1, k);
-
+        if(arr[mid]<k)
         return bs(arr, mid+1, r, k);
+
+        return bs(arr, l, mid-1, k);
     }
 
     return -1;
@@ -24,9 +23,11 @@ int main(void){
     int arr[] = {2,3,4,10,40};
     int k = 20;
     int n = sizeof(arr)/sizeof(arr[0]);
+
     int result = bs(arr, 0, n-1, k);
     (result==-1)
-        ?cout<<"element not found"
-        :cout<<"element found "<<result;
+    ?cout<<"element not found"
+    :cout<<"element found "<<result;
+
     return 0;
 }
