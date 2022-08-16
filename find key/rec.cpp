d9dcc50fffdbd97ad79f45924dee5ac6f174c4c6
ams1826 <akashsangwan1826@gmail.com>
2022-08-16 10:24:28 +0530

    p

---
+++ find key/rec.cpp
@@ -0,0 +1,33 @@
+//recursive
+#include<bits/stdc++.h>
+using namespace std;
+
+int bs(int arr[], int l, int r, int k){
+
+    if(r>=1){
+        int mid = l+(r-l)/2;
+
+        if(arr[mid]==k)
+        return mid;
+
+        if(arr[mid]<k)
+        return bs(arr, mid+1, r, k);
+
+        return bs(arr, l, mid-1, k);
+    }
+
+    return -1;
+}
+
+int main(void){
+    int arr[] = {2,3,4,10,40};
+    int k = 20;
+    int n = sizeof(arr)/sizeof(arr[0]);
+
+    int result = bs(arr, 0, n-1, k);
+    (result==-1)
+    ?cout<<"element not found"
+    :cout<<"element found "<<result;
+
+    return 0;
+}
