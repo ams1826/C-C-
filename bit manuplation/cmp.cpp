ca6cb297b813fc125f3fb41387c31585cb25c866
ams1826 <akashsangwan1826@gmail.com>
2022-08-23 13:59:32 +0530

    p

---
+++ bit manuplation/cmp.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+bool oppositesign(int x, int y){
+    return ((x^y)<0);
+}
+
+int main(){
+    int x=100 , y=-100;
+    if(oppositesign(x,y)==true)
+    cout<<"signs are oppositesign ";
+    else
+    cout<<"same sign ";
+    return 0;
+}
+
+
+// asuming that bit numbering starts from 1.
+// wap to set a particular bit in a given number
