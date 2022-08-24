f0315a984a7477a31de33c281c2b6c73a6006175
ams1826 <akashsangwan1826@gmail.com>
2022-08-25 02:24:43 +0530

    p

---
+++ bit manuplation/negate.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int num, bit, temp;
+    cout<<"enter no. ";
+    cin>>num;
+    cout<<"enter the bit num ";
+    cin>>bit;
+    temp=1<<(bit-1);
+    num = num^temp;
+    cout<<"value after setting bit "<<num;
+    return 0;
+}
+
+//      4 3 2 1
+// no 14 = 1 1 1 0
+// pos 2 = 0 0 1 0
+//              -1
+//         0 0 0 1 --> left shift
+
+//         0 0 1 0
+
+//         1 1 1 0
+//         0 0 1 0
+//        """"""""""
+//         1 1 0 0 --> 12
---
+++ bit manuplation/shift.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+#include<iostream>
+using namespace std;
+int main(){
+    int a = 3 , c;
+    c = a<<1;
+    cout<<c;
+    return 0;
+}
